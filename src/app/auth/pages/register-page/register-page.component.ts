5339c7889166382985394ad6b89ac9615711516f
Carlos DIaz <c.diazadriann@gmail.com>
2024-03-20 22:09:23 -0400

    Fin Seccion 17

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  cantBeStrider,
+  emailPattern,
+  firstNameAndLastnamePattern,
+} from '../../../shared/validators/validators';
+import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class RegisterPageComponent {
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService
+  ) {}
+  public myForm: FormGroup = this.fb.group({
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(this.validatorsService.firstNameAndLastnamePattern),
+      ],
+    ],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(this.validatorsService.emailPattern),
+      ],
+      [new EmailValidatorService()],
+    ],
+    username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
 
+  public isValidField(field: string) {
+    return this.validatorsService.isValidField(this.myForm, field);
+  }
+
+  public onSaved() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+  }
 }
--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches-page',
   templateUrl: './switches-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    //Valores iniciales
+    this.myForm.reset(this.person);
+  }
 
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  public person = {
+    gender: 'F',
+    wantNotifications: false,
+  };
+
+  onSaved() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+    this.person = newPerson;
+    console.log(this.myForm.value);
+    console.log(this.person);
+  }
+
+  public isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
 }
